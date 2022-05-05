b4b1fffc929fdbeb65ad3a15aa3a7bb652b1bdc3
Yu Yin <yxonic@gmail.com>
2022-05-05 23:08:08 +0800

    feat: basic plugin feature

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-import { add } from './add'
+import VMarkRenderer from '@vmark/core'
+import type { Plugin } from 'vite'
 
-console.log(add(3, 4))
+const mdRegex = /\.md$/
+
+export default function plugin(): Plugin {
+  const renderer = new VMarkRenderer({
+    h(name, attr, children) {
+      return {
+        text: `h("${name}", ${JSON.stringify(attr)}, [${children?.map(
+          (c) => c.text || JSON.stringify(c),
+        )}])`,
+      }
+    },
+  })
+  return {
+    name: '@vmark/vite-plugin',
+    async transform(src, id) {
+      if (!mdRegex.test(id)) {
+        return
+      }
+      let code = `import { h } from 'vue'\n`
+      const { text } = await renderer.render(src)
+      code += text
+      code += `\nexport default {}`
+      return code
+    },
+  }
+}
